db35c8a97c3c9b8c727fb4c3516bb4598f7a418d
dangercoding <98159807+dangercoding@users.noreply.github.com>
2026-05-05 10:00:07 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.java
@@ -0,0 +1,42 @@
+
+
+class Solution {
+
+    private static int base = 1;
+
+    public String encode(List<String> strs) {
+       
+         StringBuilder p = new StringBuilder();
+
+         for(String s:strs){
+
+            p.append(s.length()).append("&").append(s);
+         }
+
+         return p.toString();
+            
+    }
+
+    public List<String> decode(String str) {
+         List<String> raw = new ArrayList<>();
+
+         int i=0;
+         while(i<str.length()){
+
+            int j=i;
+            while(str.charAt(j)!= '&'){
+                j++;
+            }
+
+            int size= Integer.parseInt(str.substring(i,j));
+             j++;
+              String word= str.substring(j,j+size);
+              raw.add(word);
+              i=j+size;
+
+         }
+
+         return raw;
+        
+    }
+}
