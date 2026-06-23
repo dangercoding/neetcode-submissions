55895b676806a1d4c78f67cf57587dab5268e822
dangercoding <98159807+dangercoding@users.noreply.github.com>
2026-06-23 22:04:53 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int l = 0;
+        int maxL = 0;
+        int maxF = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int r = 0; r < s.length(); r++) {
+
+            map.put(s.charAt(r),
+                    map.getOrDefault(s.charAt(r), 0) + 1);
+
+            maxF = Math.max(maxF, map.get(s.charAt(r)));
+
+            while ((r - l + 1) - maxF > k) {
+
+                map.put(s.charAt(l),
+                        map.get(s.charAt(l)) - 1);
+
+                if (map.get(s.charAt(l)) == 0) {
+                    map.remove(s.charAt(l));
+                }
+
+                l++;
+
+                // Recalculate max frequency in current window
+                maxF = 0;
+                for (Map.Entry<Character, Integer> en : map.entrySet()) {
+                    maxF = Math.max(maxF, en.getValue());
+                }
+            }
+
+            maxL = Math.max(maxL, r - l + 1);
+        }
+
+        return maxL;
+    }
+}
